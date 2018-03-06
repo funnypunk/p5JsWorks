32a84a4941024c72aeb0c1972429f44353e92963
Kiran <kirangovindp@gmail.com>
2018-03-07 02:15:49 +0530

    spawnCircle

--- circle.js
+++ circle.js
@@ -1,36 +1,47 @@
-
-function Circle(tempX,tempY,tempRadius) {  
-    this.radius = tempRadius;    
-    this.pos = createVector(tempX,tempY);
+function Circle() {
+    this.radius;
+    this.pos = createVector('', '');
     this.angle = 0;
     this.increment = 0;
-    this.constant = 15;
+    this.constant = 9;
+    this.alive = false;
+    this.ellipseRadius = 9;
 
-    this.drawC = function() {
-        
-        push();
-        translate(this.pos.x,this.pos.y);       
-        noFill();
-        debug.log(this.pos);
-        x = this.pos.x
-        y = this.pos.y   
-        ellipse(x,y,this.increment);
-        this.rotateC();
-        pop();
-        
+    this.drawC = function () {
+        if (this.alive) {
+            for (var i = 0; i <= 30; i++) {
+                push();
+                fill(this.angle % 256, 155, 222);
 
-        
+                x = this.pos.x;
+                y = this.pos.y;
+                strokeWeight(1);
+                ellipse(x, y, this.ellipseRadius);
+
+                pop();
+                this.rotateC();
+            }
+        }
     }
-    
-    this.rotateC = function() {
-  
-        this.angle = 137.5 * this.increment;  
-        this.increment ++;
 
+    this.rotateC = function () {
+
+        this.angle = 137.5 * this.increment;
+        this.increment++;
         this.radius = this.constant * sqrt(this.increment);
+        this.pos.x = this.radius * cos(this.angle) + this.pos.x;
+        this.pos.y = this.radius * sin(this.angle) + this.pos.y;
+        if (this.radius > 400) {
+            this.alive = false;
+        }
 
-        this.pos.x = this.radius * cos(this.angle);
-        this.pos.y = this.radius * sin(this.angle);
-      
+    }
+
+    this.spawn = function (x, y) {
+        if (!this.alive) {
+            this.alive = true;
+            this.pos.x = x;
+            this.pos.y = y;
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,30 +1,28 @@
-
-
-
-
-var circle,circle1;
-var n,c;
-
+var circle,c1;
 
 function setup() {
-  createCanvas(1800,1000);
+  createCanvas(1800, 1000);
   frameRate(30);
   angleMode(DEGREES);
-  //circle = new Circle(width/4,height/2,2);
-  circle = new Circle(width/2,height/2,2);
-     
+  colorMode(RGB, 255, 255, 255, 1)
   
+
+  circle = new Circle();
+  c1 = new Circle();
+  circle.spawn(width / 2, height / 2);
+
+
+
 }
 
 function draw() {
- 
- // circle1.drawC();
-  
-  
+
+
   circle.drawC();
-  console.log(circle.pos);
-  //circle.rotateC(); 
-  
+  c1.drawC();
+
 }
 
-
+function mouseClicked() {
+  c1.spawn(mouseX,mouseY);
+}
